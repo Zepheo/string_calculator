6183556ca024a7c9898d2030473f1e42f4e72285
Zepheo <douglas.eskelius@gmail.com>
2020-12-22 09:34:25 +0100

    renamed variables

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
-const operationRegex = /^-?\d+(\s[+-/*]\s-?\d+)+$/;
+const expressionRegex = /^-?\d+(\s[+-/*]\s-?\d+)+$/;
 
-export const calculate = (operation: string): number => {
-  if (!operationRegex.test(operation)) throw new Error('Argument must have shape "(num) (operator) (num)"');
+export const calculate = (expression: string): number => {
+  if (!expressionRegex.test(expression)) throw new Error('Argument must have shape "(num) (operator) (num)"');
 
-  const operations = operation.split(' ');
+  const splitExpression = expression.split(' ');
 
-  const numbers = operations.filter((char) => !Number.isNaN(Number(char)))
+  const numbers = splitExpression.filter((char) => !Number.isNaN(Number(char)))
     .map((n) => Number(n));
 
-  const operators = operations.filter((char) => Number.isNaN(Number(char)));
+  const operators = splitExpression.filter((char) => Number.isNaN(Number(char)));
 
   while (operators.includes('*') || operators.includes('/')) {
     const indexOfMultOrDiv = operators.findIndex((op) => op === '*' || op === '/');
     const operator = operators.splice(indexOfMultOrDiv, 1)[0];
-    const firstNumber = numbers.splice(indexOfMultOrDiv, 1)[0];
-    const secondNumber = numbers.splice(indexOfMultOrDiv, 1)[0];
+    const leftNumber = numbers.splice(indexOfMultOrDiv, 1)[0];
+    const rightNumber = numbers.splice(indexOfMultOrDiv, 1)[0];
 
-    const result = operator === '*' ? firstNumber * secondNumber : firstNumber / secondNumber;
+    const result = operator === '*' ? leftNumber * rightNumber : leftNumber / rightNumber;
     numbers.splice(indexOfMultOrDiv, 0, result);
   }
 
   while (operators.length > 0) {
-    const firstNumber = numbers.shift() as number;
-    const secondNumber = numbers.shift() as number;
+    const leftNumber = numbers.shift() as number;
+    const rightNumber = numbers.shift() as number;
     const operator = operators.shift();
 
     let result: number;
 
-    if (operator === '+') result = firstNumber + secondNumber;
-    else result = firstNumber - secondNumber;
+    if (operator === '+') result = leftNumber + rightNumber;
+    else result = leftNumber - rightNumber;
     numbers.unshift(result);
   }
 
   return numbers[0];
 };
 
-export const evalCalculate = (operation: string): number => {
-  if (!operationRegex.test(operation)) throw new Error('Argument must have shape "(num) (operator) (num)"');
+export const evalCalculate = (expression: string): number => {
+  if (!expressionRegex.test(expression)) throw new Error('Argument must have shape "(num) (operator) (num)"');
 
-  return eval(operation);
+  return eval(expression);
 };
